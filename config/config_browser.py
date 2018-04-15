601c80f697be1b4a5f5bcb197b0a3c5869f9ed46
chenye3814 <13551833814@163.com>
2018-04-15 18:31:53 +0800

    下班提交

--- config/config_browser.py
+++ config/config_browser.py
@@ -2,15 +2,14 @@
 from selenium import webdriver
 
 def browser_choose(str):
-    if str=="Chrome":
+    "通过传递的参数确定选择的浏览器"
+    if str.lower()=="chrome":       #将传递的字符串全部小写进行比较，避免大小写引起启动不成功的问题
         browser = webdriver.Chrome()
-    elif str=="ie":
+    elif str.lower()=="ie":
         browser = webdriver.Ie()
-    elif str=="firefox":
+    elif str.lower()=="firefox":
         browser = webdriver.Firefox()
     else:
         print("no this driver")
-
     return browser
-
 
